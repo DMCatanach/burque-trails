424a3f05a0cc02604d15b0c565680ada5163c6f6
DMCatanach <dawn.catanach@gmail.com>
2018-07-27 16:10:06 -0600

    add option to view details about more than one park without getting stuck in a loop.

--- lib/burque_trails/cli.rb
+++ lib/burque_trails/cli.rb
@@ -10,16 +10,30 @@ class BurqueTrails::CLI
 	def menu #eventually there will also be an option to see a list of all the trails, once that's working. Then this'll get a refactor
 		puts "Please enter a ZIP code where you would like to find a walking trail:" 
 		input = gets.strip
-		puts "Here are the parks with walking trails in #{input}:" #this is where we would want to call a method to get the output
+		puts "Here are the parks with walking trails in #{input}:" #this is where we would want to call a method to get the output, passing input as argument
 		puts <<-DOC 
-		1. Arroyo del Oso 
+		1. Arroyo del Oso* 
 		2. Academy Hills 
 		3. North Domingo Baca 
+		4. Bataan Park*
 		DOC
 		puts "Enter the number of a park you would like to see more details about. Otherwise, enter 'exit.'" 
 		input = gets.strip 
-		puts "Here is some information about the park you selected: " 
-		puts "[some information scraped from the site]" 
+		unless input == "exit"
+			puts "Here is some information about the park you selected: " 
+			puts "[some information scraped from the site]" 
+			puts "Would you like to see information about another park? (y/n)"
+			input = gets.strip.downcase 
+			if input == "y" 
+				menu
+			else 
+				goodbye
+			end 
+		end
 	end 
+
+	def goodbye 
+		puts "Happy trails!" 
+	end
 
 end 
